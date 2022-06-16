c8348b23fa012cdcee71bc46aa46b5cd76da9f93
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2022-06-16 15:41:42 +0600

    alert done

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React,{useState} from 'react';
-import { View, Text, StyleSheet, Image,FlatList,Alert } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, FlatList, Alert } from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
 import uuid from 'react-native-uuid';
@@ -7,45 +7,56 @@ import AddItem from './components/AddItem';
 
 const App = () => {
 
-  const [items,setItems] = useState([
-    {id:uuid.v4(),text:'Milk'},
-    {id:uuid.v4(),text:'Eggs'},
-    {id:uuid.v4(),text:'Bread'},
-    {id:uuid.v4(),text:'Juice'},
+  const [items, setItems] = useState([
+    { id: uuid.v4(), text: 'Milk' },
+    { id: uuid.v4(), text: 'Eggs' },
+    { id: uuid.v4(), text: 'Bread' },
+    { id: uuid.v4(), text: 'Juice' },
   ]);
 
   const deleteItems = (delteItemId) => {
 
-    setItems((items) =>  items.filter(f => f.id !== delteItemId))
+    setItems((items) => items.filter(f => f.id !== delteItemId))
 
   }
 
   const addItem = (itemText) => {
-    // if(itemText){
-    //   setItems((items) => [...items,{id:uuid.v4(),text:itemText}]);
-    // }
-    // else{
-    //   Alert.alert('Error','Please enter an item',{text:'ok'}); 
-    // }
-    setItems((items) => [...items,{id:uuid.v4(),text:itemText}]);
+    if (itemText) {
+      setItems((items) => [...items, { id: uuid.v4(), text: itemText }]);
+    }
+    else {
+      Alert.alert(
+        "Text Field Empty!",
+        "Please add something",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel"
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") }
+        ]
+      );
+    }
+
 
   }
 
   return (
     <View style={styles.container}>
-      <Header title={'Shopping List'}/>
-      <AddItem addItem={addItem}/>
-      <FlatList data={items} renderItem={({item}) => (
-        <ListItem item={item} deleteItems={deleteItems}/>
+      <Header title={'Shopping List'} />
+      <AddItem addItem={addItem} />
+      <FlatList data={items} renderItem={({ item }) => (
+        <ListItem item={item} deleteItems={deleteItems} />
       )} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    paddingTop:60,
+  container: {
+    flex: 1,
+    paddingTop: 60,
   }
 });
 
