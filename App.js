ddfb6898be9a05250e5223a85ebbda28560ee026
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2022-06-16 11:52:07 +0600

    header component added

--- App.js
+++ App.js
@@ -1,30 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import Header from './components/Header';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>
-        Hello Brinto
-      </Text>
-      <Image style={styles.img} source={{ uri: 'https://randomuser.me/api/portraits/men/68.jpg' }} />
-    </View>
+    <Header/>
   );
 };
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  text: {
-    color: 'red',
-    fontSize: 50
-  },
-  img:{
-    width:100,
-    height:100,
-    borderRadius:10 / 2,
-  }
-});
+
 export default App;
